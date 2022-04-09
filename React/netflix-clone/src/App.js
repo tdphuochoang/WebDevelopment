742330a623388e414dc9a32fad372f1c5a00b663
Hoang Tran <tdll.trdphoang@gmail.com>
2022-04-09 02:39:32 -0700

    Bug Fixed

---
+++ React/netflix-clone/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Netflix Clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
+  );
+}
+
+export default App;
