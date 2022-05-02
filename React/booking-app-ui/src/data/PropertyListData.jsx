c73fb7975af8032127cee779eaf0c52d78d3a79d
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-02 16:21:49 -0700

    Updated PropertyList

--- React/booking-app-ui/src/data/PropertyListData.jsx
+++ React/booking-app-ui/src/data/PropertyListData.jsx
@@ -48,11 +48,59 @@ export const PropertyListData = [
     alt: "CottagesImg",
   },
   {
-    title: "Cottages",
-    quantity: "141,707 cottages",
+    title: "Glamping",
+    quantity: "11,643 Glamping Sites",
     image:
-      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_cottages/5e1fd9cd716f4825c6c7eac5abe692c52cc64516.jpg",
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_glamping/6e181b9e942c160f4605239be7ddc1728cbcc4c8.jpg",
 
-    alt: "CottagesImg",
+    alt: "GlampingImg",
+  },
+  {
+    title: "Serviced Apartments",
+    quantity: "33,205 serviced apartments",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_aparthotel/10e092f390b128dcff92727912299eef7824b751.jpg",
+
+    alt: "ServicedApartmentsImg",
+  },
+  {
+    title: "Vacation Homes",
+    quantity: "422,612 vacation homes",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-holidayhomes_300/604c7484d34a9e8791c2d5a0e2df4bc8c803dc7c.jpg",
+
+    alt: "VacationHomesImg",
+  },
+  {
+    title: "Guest houses",
+    quantity: "101,705 guest houses",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_guest_house/70618d86d515349ce56296a56d2eaaf283fd1542.jpg",
+
+    alt: "GuestHousesImg",
+  },
+  {
+    title: "Hostels",
+    quantity: "16,392 hostels",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_hostels/cd5489c0d29025a9ca9daa602ac1581ba042bc69.jpg",
+
+    alt: "HostelsImg",
+  },
+  {
+    title: "Motels",
+    quantity: "13,042 motels",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_motels/9673cd1b55cbc1e1cdaeae09d87d16aa9d84d5f7.jpg",
+
+    alt: "MotelsImg",
+  },
+  {
+    title: "B&Bs",
+    quantity: "196,836 B&Bs",
+    image:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_bed_and_breakfast/a6a4a3f904284337c187771d94a4bb6169b168d7.jpg",
+
+    alt: "BBsImg",
   },
 ];
