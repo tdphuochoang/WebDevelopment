5a0f467956811ded49fae8c0cfde1682f6abc0c9
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-04 22:53:25 -0700

    Update list components

--- React/booking-app-ui/src/pages/list/List.jsx
+++ React/booking-app-ui/src/pages/list/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./list.css";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 
@@ -7,6 +7,22 @@ const List = () => {
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
