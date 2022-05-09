1b568dd469305abe961fe438877a4f455f67d7ba
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-08 22:38:38 -0700

    Add hotel data

--- React/booking-app-ui/src/pages/hotel/Hotel.jsx
+++ React/booking-app-ui/src/pages/hotel/Hotel.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import "./Hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { HotelImageData } from "../../data/HotelImageData";
 
 const Hotel = () => {
-  return <div>Hotel</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">The Mediterranean Inn</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New York</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location - 500m from center
+          </span>
+          <span className="hotelPriceHighLight">
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+          <div className="hotelImages"></div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+              <p>This is the message</p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hotel;
