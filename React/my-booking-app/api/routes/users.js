52d80f28f065317e887a0fa061e6e36474b456d1
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-13 13:42:09 -0700

    Updated checkUser and checkAdmin methods

--- React/my-booking-app/api/routes/users.js
+++ React/my-booking-app/api/routes/users.js
@@ -1,5 +1,33 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello user, you are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("Hello user, you are logged in and you can delete your account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Hello admin, you are logged in and you can delete all account");
+});
+
+//UPDATE
+router.put("/:id", updateUser);
+//DELETE
+router.delete("/:id", deleteUser);
+//GET
+router.get("/:id", getUser);
+//GET ALL
+router.get("/", getUsers);
 
 export default router;
