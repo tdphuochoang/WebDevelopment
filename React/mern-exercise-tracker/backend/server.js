323f86e1eece1c974d3244cf48107d1c2f088757
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-14 19:48:30 -0700

    Initilizing mern-exercise-tracker

---
+++ React/mern-exercise-tracker/backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const connect = async () => {
+  const uri = process.env.ATLAS_URI;
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+//mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//Start the server
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on port: ${port}`);
+});
