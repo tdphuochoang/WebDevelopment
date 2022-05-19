f82fa78e34750af69e4019812dabca58ab4e3605
Hoang Tran <tdll.trdphoang@gmail.com>
2022-05-19 14:25:13 -0700

    Updated Room.js

--- React/my-booking-app/api/routes/rooms.js
+++ React/my-booking-app/api/routes/rooms.js
@@ -1,5 +1,24 @@
 import express from "express";
+import {
+  createRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GET ALL
+router.get("/", getRooms);
 
 export default router;
