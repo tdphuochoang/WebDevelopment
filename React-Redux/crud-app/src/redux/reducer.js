b9f97e1026db8f0c497e7476aaa788e371df6b0c
Hoang <tdll.trdphoang@gmail.com>
2022-07-16 07:29:31 -0700

    Initilize redux files and Home page

---
+++ React-Redux/crud-app/src/redux/reducer.js
@@ -0,0 +1,17 @@
+
+const initialState = {
+    users: [],
+    user: {
+
+    },
+    loading: false,
+}
+
+const usersReducers = (state = initialState, action) => {
+    switch(action.type){ 
+        default: 
+            return state;
+    }
+}
+
+export default usersReducers;
