b4ae14aac06a8022a6980196e5957f9e616297c7
Hoang <tdll.trdphoang@gmail.com>
2022-07-21 13:33:30 -0700

    Finished CRUD operations and Search engines

--- React-Redux/mern-youtube-clone/server/routes/videos.js
+++ React-Redux/mern-youtube-clone/server/routes/videos.js
@@ -1,7 +1,18 @@
 import express from "express";
+import { addVideo, addView, getByTag, random, search, sub, trend } from "../controllers/video.js"
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.get("/")
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", addVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub", verifyToken, sub)
+router.get("/tags", getByTag) //search video by tags
+router.get("/search", search) //search video by title
 
 export default router;
