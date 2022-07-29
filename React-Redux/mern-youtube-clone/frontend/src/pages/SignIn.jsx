a65f3ecccaa89de169bcbff34b5f2c3761cd6481
Hoang <tdll.trdphoang@gmail.com>
2022-07-29 13:46:10 -0700

    Implemented Google Sign In

--- React-Redux/mern-youtube-clone/frontend/src/pages/SignIn.jsx
+++ React-Redux/mern-youtube-clone/frontend/src/pages/SignIn.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Container = styled.div`
 	display: flex;
@@ -83,6 +85,25 @@ const SignIn = () => {
 		}
 	};
 
+	const signInWithGoogle = async () => {
+		dispatch(loginStart());
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				console.log(result);
+				axios
+					.post("/auth/google", {
+						name: result.user.displayName,
+						email: result.user.email,
+						img: result.user.photoURL,
+					})
+					.then((res) => {
+						dispatch(loginSuccess(res.data));
+					});
+			})
+			.catch((error) => {
+				dispatch(loginFailure());
+			});
+	};
 	return (
 		<Container>
 			<Wrapper>
@@ -98,6 +119,8 @@ const SignIn = () => {
 					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<Button onClick={handleLogin}>Sign in</Button>
+				<Title>or</Title>
+				<Button onClick={signInWithGoogle}>Sign in with Google</Button>
 				<Title>or</Title>
 				<Input
 					placeholder="username"
