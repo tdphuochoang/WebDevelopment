a43ea6c6e6a98f3547d9135543e59886b673f4d4
Hoang <tdll.trdphoang@gmail.com>
2022-08-01 06:32:25 -0700

    Completed functionalities

--- React-Redux/mern-youtube-clone/frontend/src/components/Navbar.jsx
+++ React-Redux/mern-youtube-clone/frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import Upload from "./Upload";
 
 const Container = styled.div`
 	position: sticky;
@@ -31,11 +32,13 @@ const Search = styled.div`
 	padding: 5px;
 	border: 1px solid #ccc;
 	border-radius: 3px;
+	color: ${({ theme }) => theme.text};
 `;
 
 const Input = styled.input`
 	border: none;
 	background: transparent;
+	outline: none;
 `;
 
 const Button = styled.button`
@@ -68,31 +71,39 @@ const Avatar = styled.img`
 `;
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
+	const [q, setQ] = useState("");
 	const { currentUser } = useSelector((state) => state.user);
-
+	const navigate = useNavigate();
 	return (
-		<Container>
-			<Wrapper>
-				<Search>
-					<Input placeholder="Search" />
-					<SearchOutlinedIcon />
-				</Search>
-				{currentUser ? (
-					<User>
-						<VideoCallOutlinedIcon />
-						<Avatar src={currentUser.img} />
-						{currentUser.name}
-					</User>
-				) : (
-					<Link to="signin" style={{ textDecoration: "none" }}>
-						<Button>
-							<AccountCircleOutlinedIcon />
-							SIGN IN
-						</Button>
-					</Link>
-				)}
-			</Wrapper>
-		</Container>
+		<>
+			<Container>
+				<Wrapper>
+					<Search>
+						<Input
+							placeholder="Search"
+							onChange={(e) => setQ(e.target.value)}
+						/>
+						<SearchOutlinedIcon onClick={() => navigate(`/search?q=${q}`)} />
+					</Search>
+					{currentUser ? (
+						<User>
+							<VideoCallOutlinedIcon onClick={() => setOpen(true)} />
+							<Avatar src={currentUser.img} />
+							{currentUser.name}
+						</User>
+					) : (
+						<Link to="signin" style={{ textDecoration: "none" }}>
+							<Button>
+								<AccountCircleOutlinedIcon />
+								SIGN IN
+							</Button>
+						</Link>
+					)}
+				</Wrapper>
+			</Container>
+			{open && <Upload setOpen={setOpen} />}
+		</>
 	);
 };
 
--- React-Redux/mern-youtube-clone/frontend/src/pages/Video.jsx
+++ React-Redux/mern-youtube-clone/frontend/src/pages/Video.jsx
@@ -14,6 +14,7 @@ import axios from "axios";
 import { dislike, fetchSuccess, like } from "../redux/videoSlice";
 import { format } from "timeago.js";
 import { subscription } from "../redux/userSlice";
+import Recommendation from "../components/Recommendation";
 
 const Container = styled.div`
 	display: flex;
@@ -59,10 +60,6 @@ const Button = styled.div`
 const Hr = styled.hr`
 	margin: 15px 0px;
 	border: 0.5px solid ${({ theme }) => theme.soft};
-`;
-
-const Recommendation = styled.div`
-	flex: 2;
 `;
 
 const Channel = styled.div`
@@ -164,7 +161,7 @@ const Video = () => {
 			{currentVideo && (
 				<Content>
 					<VideoWrapper>
-						<VideoFrame src={currentVideo.videoUrl} />
+						<VideoFrame src={currentVideo.videoUrl} controls />
 					</VideoWrapper>
 					<Title>{currentVideo.title}</Title>
 					<Details>
@@ -216,18 +213,7 @@ const Video = () => {
 					<Comments videoId={currentVideo._id} />
 				</Content>
 			)}
-			{/* <Recommendation>
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-				<Card type="sm" />
-			</Recommendation> */}
+			<Recommendation tags={currentVideo.tags} />
 		</Container>
 	);
 };
